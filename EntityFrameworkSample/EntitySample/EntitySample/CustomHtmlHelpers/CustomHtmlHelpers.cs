f03f44c0fc26f4e3c66e3e17e61c0df2a269a85e
sreedevi vp <sreedevivp4u@gmail.com>
2016-09-03 16:50:03 +0530

    learning

---
+++ EntityFrameworkSample/EntitySample/EntitySample/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntitySample.CustomHtmlHelpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper,string src,string alt)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            if (string.IsNullOrEmpty(src))
+            {
+                src = "";
+                tb.Attributes.Add("src", src);
+                
+            }
+            else
+            {
+                tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            }
+            
+            tb.Attributes.Add("alt", alt);
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
