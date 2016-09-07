3d553ce54749e33dea211c255431260e952ea19f
sreedevi vp <sreedevivp4u@gmail.com>
2016-09-07 09:19:57 +0530

    Android app learning

---
+++ AppDevelopment/AndroidStudioProjects/MyApplication/app/src/main/java/com/example/sreekumar/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sreekumar.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
